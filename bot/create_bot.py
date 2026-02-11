c84643d9694cfe120c1eb64ac00167cbd3ac2e1d
sanbob <saintsanbob@gmail.com>
2026-02-11 11:16:26 +0300

    fix: logging time format

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -7,6 +7,7 @@ from config import config
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
 
